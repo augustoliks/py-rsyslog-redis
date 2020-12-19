9dec3ab695aa6c885218a8c96ff0cc1ce4af7426
augustoliks <carlos.neto.dev@gmail.com>
2020-12-19 20:35:55 -0300

    doc: sphinx update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,12 +25,11 @@ author = 'Carlos Neto <carlos.neto.dev@gmail.com>'
 
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    'sphinx.ext.*',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.napoleon',
     'sphinx_rtd_theme'
 ]
 
